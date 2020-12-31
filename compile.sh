4429e6315226d07ec07c38f40471251e59b162f8
David Reguera Garcia aka Dreg <dreg@fr33project.org>
2020-12-31 06:43:27 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -21,5 +21,5 @@
 # IN THE SOFTWARE.
 
 rm -rf reverse_shell*
-nasm.exe -f win32 reverse_asm.asm -o reverse_shell.o
+nasm -f win32 reverse_asm.asm -o reverse_shell.o
 ld -mi386pe reverse_shell.o -o reverse_shell32.exe
